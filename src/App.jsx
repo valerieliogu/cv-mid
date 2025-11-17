9a95d1abb6a7bce3585bde141c8d7c3531f4c6a3
valerieliogu <valerieliogu23@gmail.com>
2025-11-17 21:59:23 +0800

    toast fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-// src/App.jsx
-import React from "react";
-
-import { ThemeProvider } from "@/components/theme-provider";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/toast";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
@@ -27,35 +20,27 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <TooltipProvider>
-          <Toaster />
-          <Sonner />
-
-          <BrowserRouter>
-            <div className="flex flex-col min-h-screen">
-              <Navbar />
-
-              <main className="flex-1 pt-16">
-                <Routes>
-                  <Route path="/" element={<Home />} />
-                  <Route path="/about" element={<About />} />
-                  <Route path="/education" element={<Education />} />
-                  <Route path="/skills" element={<Skills />} />
-                  <Route path="/experience" element={<Experience />} />
-                  <Route path="/contact" element={<Contact />} />
-                  <Route path="*" element={<NotFound />} />
-                </Routes>
-              </main>
-
-              <Footer />
-            </div>
-          </BrowserRouter>
-        </TooltipProvider>
-      </ThemeProvider>
-
-      {/* React Query Devtools */}
-      <ReactQueryDevtools initialIsOpen={false} />
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-1 pt-16">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/education" element={<Education />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/experience" element={<Experience />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </TooltipProvider>
     </QueryClientProvider>
   );
 };
