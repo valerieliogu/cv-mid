eb67b1b904ba54fd2b61d53bff5f55cbf9742cdc
valerieliogu <valerieliogu23@gmail.com>
2025-11-17 21:45:10 +0800

    add installation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+// src/App.jsx
 import React from "react";
+
+import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { ThemeProvider } from "@/components/theme-provider";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
@@ -27,9 +31,11 @@ const App = () => {
         <TooltipProvider>
           <Toaster />
           <Sonner />
+
           <BrowserRouter>
             <div className="flex flex-col min-h-screen">
               <Navbar />
+
               <main className="flex-1 pt-16">
                 <Routes>
                   <Route path="/" element={<Home />} />
@@ -41,12 +47,14 @@ const App = () => {
                   <Route path="*" element={<NotFound />} />
                 </Routes>
               </main>
+
               <Footer />
             </div>
           </BrowserRouter>
         </TooltipProvider>
       </ThemeProvider>
 
+      {/* React Query Devtools */}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
